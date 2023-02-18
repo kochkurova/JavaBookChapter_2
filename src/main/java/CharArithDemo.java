85a1bb25cdcfa03a451810dbe7cdb24e7a1bdc3b
Elena <kochkurova.en@gmail.com>
2023-02-18 19:34:09 +0600

    глава_2

---
+++ src/main/java/CharArithDemo.java
@@ -0,0 +1,24 @@
+public class CharArithDemo {
+    public static void main (String args []) {
+
+
+
+
+        char ch;
+        ch = 'x';
+        System.out.println("ch это символ " + ch);
+
+        ch ++;
+        System.out.println("теперь ch это " + ch);
+
+        ch = 90;
+        System.out.println("теперь ch это " + ch);
+
+        ch = 92;
+        System.out.println("теперь cr это " + ch);
+
+
+
+
+    }
+}
